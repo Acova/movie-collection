0a70494b6e06235414a5eb9f30a3a202a84587e9
Hector Acosta Valido <hector.acosta.valido@outlook.es>
2025-06-21 17:18:14 +0200

    Added a basic hello-world endpoint to start working on dockerization

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/hello-world", helloWorld)
+	router.Run("localhost:8080")
+}
+
+func helloWorld(c *gin.Context) {
+	c.Data(http.StatusOK, "application/json; charset=utf-8", []byte("Hola, mundo!"))
+}
